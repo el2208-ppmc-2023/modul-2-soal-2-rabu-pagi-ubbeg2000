8fb87e9ded264575f4a1ba9a682016e6faffa6ab
Elkhan <74042126+ubbeg2000@users.noreply.github.com>
2023-02-14 16:13:39 +0700

    Update main.c

--- main.c
+++ main.c
@@ -10,11 +10,59 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main() 
-{
-    // printf("Masukkan nama file: ");
-    // printf("File tidak ditemukan! \n");
-    // printf("Posisi akhir: (%0.2f, %0.2f)\n");
-    
+void readFile(FILE *fp) {
+    float x = 0;
+    float y = 0;
+    char command[20], temp[20], validate[6];  
+    float value;
+    char prevCommand[20] = "";
+
+    while (fscanf(fp, "%s", temp) != EOF) {
+        sscanf(temp, "%[^,],%[^,],%f", validate, command, &value);
+        if (strcmp(validate, "gukguk") == 0) {
+            if (strcmp(command, "move") == 0) {
+            y += value;
+            }
+            else if (strcmp(command, "moverl") == 0) {
+                x += value;
+                strcpy(prevCommand, "moverl");//checking untuk obstacle ketika command sebelumnya moverl
+            }
+            else if (strcmp(command, "obstacle") == 0) {
+                if (strcmp(prevCommand, "moverl") == 0) {
+                    y += 1;
+                    x += 1;
+                    y -= 1;
+                } else { // masuk kondisi else ketika moverl tidak command sebelumnya
+                    x += 1;
+                    y += 1;
+                    x -= 1;
+                }
+            }
+            else if (strcmp(command, "stop") == 0) {
+                printf("Posisi akhir: (%0.2f, %0.2f)\n", x, y);
+                break;
+            }
+        } else {
+            continue;
+        }
+        
+    }
+}
+
+int main() {
+    FILE *fp;
+    char namaFile[128];
+
+    printf("Masukkan nama file: ");
+    scanf("%s",namaFile);
+
+    fp = fopen(namaFile, "r");
+
+    if (fp == NULL) {
+        printf("File tidak ditemukan! \n");
+        return 1;
+    }
+    readFile(fp);
+    fclose(fp);
     return 0;
 }
